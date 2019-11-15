697faf4e245834818395170399e67b9110354780
pgm-n117 <56075103+pgm-n117@users.noreply.github.com>
2019-11-15 12:47:11 +0000

    Cambiado el fichero de generación de datos
    Cambiado el fichero de generación de datos por el que editó Dani para realizar 25 ejecuciones por tamaño y n coches, con las semillas generadas para la repetición de los casos.

--- scriptDatos.py
+++ scriptDatos.py
@@ -1,5 +1,6 @@
 from time import time
 import os
+import random
 from Estructuras.Solucion import *
 from Algoritmos.Anchura import *
 from Algoritmos.Profundidad import *
@@ -11,6 +12,17 @@ tiempoInicio = None
 tiempoFin = None
 solucion = Solucion(None, None, None, None, None, None, None)
 cabecera = "Tamaño,NCoches,Coste,Nodos Generados, Nodos Expandidos, Nodos Explorados, Max. nodos en memoria, Tiempo de ejecucion\n"
+tmax = 600
+Npruebas = 25 #numero de pruebas que se haran para cada caso
+
+seeds = []
+random.seed(2019)
+for j in range(3):
+    seeds.append([])
+    for i in range(6):
+        seeds[j].append([])
+        for n in range(Npruebas):
+            seeds[j][i].append(random.random())
 
 '''
 Formato del fichero csv:
@@ -21,68 +33,86 @@ if(not(os.path.exists("./Datos"))):
     os.mkdir('./Datos', 0o777)
 
 
+contador = 0
 #Ejecución en anchura
 fd = open("./Datos/Anchura.csv", "w")
 fd.writelines(cabecera)
-for i in range(5, 10+1):    #Tamaño del maze
-    for j in range(1,3+1):
-        for n in range(5):  #5 veces para cada configuracion, para hacer la media de las estadisticas
+for j in range(1,3+1):    #Tamaño del maze
+    for i in range(5, 10+1):
+        for n in range(Npruebas):  #5 veces para cada configuracion, para hacer la media de las estadisticas
             tiempoInicio = time()
-            solucion = Anchura(i, j, time())    #usamos time para generar en Maze una semilla aleatoria con random.seed(time)
+            solucion = Anchura(i, j, seeds[j-1][i-5][n])    #usamos las semillas para generar los problemas
             tiempoFin = (time()-tiempoInicio)
             fd.writelines(i.__str__() + "," + j.__str__() + "," + str(solucion.coste) + "," + str(solucion.nCreados) + "," + str(solucion.nExpan) + "," + str(solucion.nExpl) + "," + str(solucion.maxNodos) + "," + str(tiempoFin)+"\n")
+            contador+=tiempoFin
+            if(contador>tmax): break
+        if(contador>tmax): break
+    if(contador>tmax): break
 fd.close()
 
-
+contador = 0
 #Ejecución en profundidad
 fd = open("./Datos/Profundidad.csv", "w")
 fd.writelines(cabecera)
-for i in range(5, 10+1):    #Tamaño del maze
-    for j in range(1,3+1):
-        for n in range(5):  #5 veces para cada configuracion, para hacer la media de las estadisticas
+for j in range(1,3+1):    #Tamaño del maze
+    for i in range(5, 10+1):
+        for n in range(Npruebas):  #5 veces para cada configuracion, para hacer la media de las estadisticas
             tiempoInicio = time()
-            solucion = Profundidad(i, j, time())    #usamos time para generar en Maze una semilla aleatoria con random.seed(time)
+            solucion = Profundidad(i, j, seeds[j-1][i-5][n],-1)    #usamos las semillas para generar los problemas
             tiempoFin = (time()-tiempoInicio)
             fd.writelines(i.__str__() + "," + j.__str__() + "," + str(solucion.coste) + "," + str(solucion.nCreados) + "," + str(solucion.nExpan) + "," + str(solucion.nExpl) + "," + str(solucion.maxNodos) + "," + str(tiempoFin)+"\n")
+            contador+=tiempoFin
+            if(contador>tmax): break
+        if(contador>tmax): break
+    if(contador>tmax): break
 fd.close()
 
 
 #Ejecución en profundidad con limite
 
-
+contador = 0
 #Ejecución Coste uniforme
-d = open("./Datos/CUniforme.csv", "w")
+fd = open("./Datos/CUniforme.csv", "w")
 fd.writelines(cabecera)
-for i in range(5, 10+1):    #Tamaño del maze
-    for j in range(1,3+1):
-        for n in range(5):  #5 veces para cada configuracion, para hacer la media de las estadisticas
+for j in range(1,3+1):    #Tamaño del maze
+    for i in range(5, 10+1):
+        for n in range(Npruebas):  #5 veces para cada configuracion, para hacer la media de las estadisticas
             tiempoInicio = time()
-            solucion = costeUniforme(i, j, time())    #usamos time para generar en Maze una semilla aleatoria con random.seed(time)
+            solucion = costeUniforme(i, j, seeds[j-1][i-5][n])    #usamos las semillas para generar los problemas
             tiempoFin = (time()-tiempoInicio)
             fd.writelines(i.__str__() + "," + j.__str__() + "," + str(solucion.coste) + "," + str(solucion.nCreados) + "," + str(solucion.nExpan) + "," + str(solucion.nExpl) + "," + str(solucion.maxNodos) + "," + str(tiempoFin)+"\n")
+            contador+=tiempoFin
+            if(contador>tmax): break
+        if(contador>tmax): break
+    if(contador>tmax): break
 fd.close()
 
+contador = 0
 #Ejecución Primero Mejor
-d = open("./Datos/PMejor.csv", "w")
+fd = open("./Datos/PMejor.csv", "w")
 fd.writelines(cabecera)
-for i in range(5, 10+1):    #Tamaño del maze
-    for j in range(1,3+1):
-        for n in range(5):  #5 veces para cada configuracion, para hacer la media de las estadisticas
+for j in range(1,3+1):    #Tamaño del maze
+    for i in range(5, 10+1):
+        for n in range(Npruebas):  #5 veces para cada configuracion, para hacer la media de las estadisticas
             tiempoInicio = time()
-            solucion = primeroMejor(i, j, time())    #usamos time para generar en Maze una semilla aleatoria con random.seed(time)
+            solucion = primeroMejor(i, j, seeds[j-1][i-5][n])    #usamos las semillas para generar los problemas
             tiempoFin = (time()-tiempoInicio)
             fd.writelines(i.__str__() + "," + j.__str__() + "," + str(solucion.coste) + "," + str(solucion.nCreados) + "," + str(solucion.nExpan) + "," + str(solucion.nExpl) + "," + str(solucion.maxNodos) + "," + str(tiempoFin)+"\n")
 fd.close()
 
-
+contador = 0
 #Ejecución A*
-d = open("./Datos/AEstrella.csv", "w")
+fd = open("./Datos/AEstrella.csv", "w")
 fd.writelines(cabecera)
-for i in range(5, 10+1):    #Tamaño del maze
-    for j in range(1,3+1):
-        for n in range(5):  #5 veces para cada configuracion, para hacer la media de las estadisticas
+for j in range(1,3+1):    #Tamaño del maze
+    for i in range(5, 10+1):
+        for n in range(Npruebas):  #5 veces para cada configuracion, para hacer la media de las estadisticas
             tiempoInicio = time()
-            solucion = AEstrella(i, j, time())    #usamos time para generar en Maze una semilla aleatoria con random.seed(time)
+            solucion = AEstrella(i, j, seeds[j-1][i-5][n])    #usamos las semillas para generar los problemas
             tiempoFin = (time()-tiempoInicio)
             fd.writelines(i.__str__() + "," + j.__str__() + "," + str(solucion.coste) + "," + str(solucion.nCreados) + "," + str(solucion.nExpan) + "," + str(solucion.nExpl) + "," + str(solucion.maxNodos) + "," + str(tiempoFin)+"\n")
+            contador+=tiempoFin
+            if(contador>tmax): break
+        if(contador>tmax): break
+    if(contador>tmax): break
 fd.close()
